af5dc0d509d857ba5a7c42a30b3c5f4212a5b15b
Pablo Ruan <Pablo@Mac-Pro-de-Pablo.local>
2015-02-11 12:51:55 -0200

    Initial Commit

---
+++ ControlAulas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ControlAulas
+//
+//  Created by Pablo Ruan on 2/11/15.
+//  Copyright (c) 2015 Pablo Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
